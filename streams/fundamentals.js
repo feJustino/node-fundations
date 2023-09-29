cd8ff26e2cab5c43bee0b61e8cc62409eab2beed
FeJustino <fwxjustino@gmail.com>
2023-09-29 15:32:28 -0300

    feat:  fundamentals of stream nodeJS

---
+++ streams/fundamentals.js
@@ -0,0 +1,42 @@
+// Stream => leitura de arquivo em partes, baseado em buffer
+// Streams --> 
+// process.stdin.pipe(process.stdout)
+import {Readable, Writable, Transform} from 'node:stream'
+
+export class OneToHundredStream extends Readable {
+
+  index = 1
+
+  _read(){
+    const i = this.index ++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buf = Buffer.from(String(i))
+        this.push(buf)
+      }
+    }, 1000);
+  }
+}
+
+export class InverseNumberStream extends Transform {
+  _transform(chunk, enconding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+
+export class MultiplyByTenStream extends Writable {
+
+  _write(chunk, enconding, callback) {
+    console.log(Number(chunk.toString())*10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream)
+  .pipe(new MultiplyByTenStream())
