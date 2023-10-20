d883ef263b62335f39d93f7b000124905b35c95c
feJustino <fwxjustino@gmail.com>
2023-10-20 00:51:25 -0300

    feat: create update and delete in DB

--- src/database.js
+++ src/database.js
@@ -1,5 +1,4 @@
 import fs from 'node:fs/promises'
-import { url } from 'node:inspector'
 
 const databasePath = new URL("../db.json", import.meta.url)
 
@@ -8,19 +7,20 @@ export class Database {
   #database = {}
 
   constructor() {
-    fs.readFile(databasePath, 'utf8').then(data => {
-      this.#database = JSON.parse(data)
-    }).catch(() => {
-      this.#persist()
-    })
+    fs.readFile(databasePath, 'utf8')
+      .then(data => {
+        this.#database = JSON.parse(data)
+      }).catch(() => {
+        this.#persist()
+      })
   }
 
   #persist() {
-    fs.writeFile("db.json", JSON.stringify(this.database))
+    fs.writeFile("db.json", JSON.stringify(this.#database))
   }
 
   select(table) {
-    const data = this.database[table] ?? []
+    const data = this.#database[table] ?? []
 
     return data
 
@@ -28,14 +28,39 @@ export class Database {
 
   insert(table, data) {
 
-    if (Array.isArray(this.database[table])) {
+    if (Array.isArray(this.#database[table])) {
 
-      this.database[table].push(data)
+      this.#database[table].push(data)
 
     } else {
-      this.database[table] = data
+      this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
+
+  update(table, id, data) {
+
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = {id, ...data}
+      this.#persist()
+    }
+
+  }
+
+  delete(table, id) {
+
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    } 
+
+  }
+
 }
